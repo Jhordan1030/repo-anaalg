ac3131b22ba09661e59aaaa5166380a3b0f1a3ea
JhordanH <jhordanhuera2002@gmail.com>
2025-05-17 23:44:52 -0500

    cambios en el main

--- ExamenGrupalADA/src/examengrupalada/ExamenGrupalADA.java
+++ ExamenGrupalADA/src/examengrupalada/ExamenGrupalADA.java
@@ -16,32 +16,6 @@ public class ExamenGrupalADA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese el número de filas para la matriz:");
-        int filas = scanner.nextInt();
-
-        System.out.println("Ingrese el número de columnas para la matriz:");
-        int columnas = scanner.nextInt();
-
-        OrdenamientoShellSort shellSort = new OrdenamientoShellSort(filas, columnas);
-
-        System.out.println("\nMatriz original:");
-        shellSort.imprimirMatriz();
-
-        // Ordenar por filas
-
-        System.out.println("\nMatriz ordenada por filas:");
-        shellSort.imprimirOrdenamiento(shellSort.ordenarPorFilas());
-        
-        // Ordenar por columnas
-        System.out.println("\nMatriz ordenada por columnas:");
-        shellSort.imprimirOrdenamiento(shellSort.ordenarPorColumnas());
-        
-        // Ordenar matriz completa
-        System.out.println("\nMatriz completamente ordenada:");
-        shellSort.imprimirOrdenamiento(shellSort.ordenarMatrizCompleta());
-        
-        scanner.close();
     }
 }
--- ExamenGrupalADA/src/examengrupalada/OrdenamientoShellSort.java
+++ ExamenGrupalADA/src/examengrupalada/OrdenamientoShellSort.java
@@ -4,6 +4,8 @@
  */
 package examengrupalada;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Giuliana Espinoza
@@ -92,5 +94,34 @@ public class OrdenamientoShellSort extends Matriz {
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el número de filas para la matriz:");
+        int filas = scanner.nextInt();
+
+        System.out.println("Ingrese el número de columnas para la matriz:");
+        int columnas = scanner.nextInt();
+
+        OrdenamientoShellSort shellSort = new OrdenamientoShellSort(filas, columnas);
+
+        System.out.println("\nMatriz original:");
+        shellSort.imprimirMatriz();
+
+        // Ordenar por filas
+
+        System.out.println("\nMatriz ordenada por filas:");
+        shellSort.imprimirOrdenamiento(shellSort.ordenarPorFilas());
+
+        // Ordenar por columnas
+        System.out.println("\nMatriz ordenada por columnas:");
+        shellSort.imprimirOrdenamiento(shellSort.ordenarPorColumnas());
+
+        // Ordenar matriz completa
+        System.out.println("\nMatriz completamente ordenada:");
+        shellSort.imprimirOrdenamiento(shellSort.ordenarMatrizCompleta());
+
+        scanner.close();
     }
 }
