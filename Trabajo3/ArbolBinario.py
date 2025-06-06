62814fb15726f851d62dde04a65d6f239c43fe9f
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2025-06-06 11:39:26 -0500

    Evaluación de tiempos
    ejercicios 9 y 10

--- Trabajo3/ArbolBinario.py
+++ Trabajo3/ArbolBinario.py
@@ -54,9 +54,27 @@ class ArbolBinario:
         if nodo is None:
             return 0
         if nodo.get_izquierdo() is None and nodo.get_derecho() is None:
-            return 1
-        return self._contar_hojas_rec(nodo.get_izquierdo()) + self._contar_hojas_rec(nodo.get_derecho())
-
+            return 1 #a
+        return self._contar_hojas_rec(nodo.get_izquierdo()) + self._contar_hojas_rec(nodo.get_derecho()) #b+2T(n/2)
+    #n*a+b(n-1)
+    def contar_hojas_secuencial(self): #1
+        if self.raiz is None: #1
+            return 0
+        hojas = 0 #1
+        stack = [self.raiz] #1
+        #8n+1
+        while stack: #n+1 n
+            nodo = stack.pop() #2n
+            if nodo.get_izquierdo() is None and nodo.get_derecho() is None:
+                hojas += 1
+            if nodo.get_derecho() is not None: #2n
+                stack.append(nodo.get_derecho())
+            if nodo.get_izquierdo() is not None: #2n
+                stack.append(nodo.get_izquierdo())
+        return hojas #1
+        #8n+6
+    #fin ejercicio 9
+    
     #ejercicio 10 - Pablo Jiménez
     def camino_a_nodo(self, valor):
         camino = []
@@ -64,18 +82,44 @@ class ArbolBinario:
             return camino
         else:
             return None
-
+        
     def _camino_a_nodo_rec(self, nodo, valor, camino):
         if nodo is None:
             return False
         camino.append(nodo.get_valor())
         if nodo.get_valor() == valor:
-            return True
+            return True #a
         if (self._camino_a_nodo_rec(nodo.get_izquierdo(), valor, camino) or
             self._camino_a_nodo_rec(nodo.get_derecho(), valor, camino)):
-            return True
+            return True #b+2T(n/2)
         camino.pop()
         return False
+    
+    def camino_a_nodo_secuencial(self, valor): #2
+        if self.raiz is None: #1
+            return None
+        stack = [(self.raiz, [self.raiz.get_valor()])] #1
+        while stack: #n+1 n
+            nodo, camino = stack.pop() #2n
+            if nodo.get_valor() == valor:
+                return camino
+            if nodo.get_derecho() is not None: #2n
+                stack.append((nodo.get_derecho(), camino + [nodo.get_derecho().get_valor()]))
+            if nodo.get_izquierdo() is not None: #2n
+                stack.append((nodo.get_izquierdo(), camino + [nodo.get_izquierdo().get_valor()]))
+        return None #1
+    #8n+6
+    #fin ejercicio 10
+    #Evaluar tiempos:
+    #a
+    #b+2T(n/2)
+
+    #T(1) = a
+    #T(2) = b+2a
+    #T(4) = b+2(b+2a) = 3b+4a
+    #T(8) = 7b+8a
+    #T(16) = 15b+16a
+    #T(n) = n*a+b(n-1)
     
     #imprimir arbol
     def imprimir_arbol(self):
