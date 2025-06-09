8fb2f93d2637e3ae4d8690e901e8d6265b92d0cf
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2025-06-09 07:41:00 -0500

    Create Matriz.py
    Taller 6

---
+++ Talller6/Matriz.py
@@ -0,0 +1,77 @@
+import random
+class Matriz:
+    def __init__(self, matriz=None, filas=None, columnas=None):
+        if matriz is not None:
+            self.matriz = matriz
+        elif filas is not None and columnas is not None:
+            self.matriz = [[0 for _ in range(columnas)] for _ in range(filas)]
+        elif filas is not None:
+            self.matriz = [[0 for _ in range(filas)] for _ in range(filas)]
+        else:
+            self.matriz = [[0 for _ in range(3)] for _ in range(3)]
+
+    def get_matriz(self):
+        return self.matriz
+
+    def set_matriz(self, matriz):
+        self.matriz = matriz
+
+    def filas(self):
+        return len(self.matriz)
+
+    def columnas(self):
+        return len(self.matriz[0])
+
+    def get_valor(self, m, n):
+        return self.matriz[m][n]
+
+    def ingresar(self, num, fil, col):
+        self.matriz[fil][col] = num
+        
+    def fibonacci(self, n):
+        if n <= 1:
+            return n
+        return self.fibonacci(n - 1) + self.fibonacci(n - 2)
+    
+    def llenar_aleatorio(self, min=1, max=100):
+        for fila in range(len(self.matriz)):
+            for columna in range(len(self.matriz[0])):
+                self.matriz[fila][columna] = random.randint(min, max)
+
+    def sumar_columnas(self):
+        suma = [0] * len(self.matriz[0])
+        for columna in range(len(self.matriz[0])):
+            for fila in range(len(self.matriz)):
+                suma[columna] += self.matriz[fila][columna]
+        return suma
+
+    def sumar_filas(self):
+        suma = [0] * len(self.matriz)
+        for fila in range(len(self.matriz)):
+            for columna in range(len(self.matriz[0])):
+                suma[fila] += self.matriz[fila][columna]
+        return suma
+
+    def sumar_filas_recursivo(self):
+        suma = [0] * len(self.matriz)
+        def sumar_fila(idx):
+            if idx == len(self.matriz):
+                return
+            suma[idx] = self._sumar_fila_rec(idx, 0)
+            sumar_fila(idx + 1)
+        def _sumar_fila_rec(fila, col):
+            if col == len(self.matriz[0]):
+                return 0
+            return self.matriz[fila][col] + _sumar_fila_rec(fila, col + 1)
+        self._sumar_fila_rec = _sumar_fila_rec  # Para acceso interno
+        sumar_fila(0)
+        return suma
+    
+    def __str__(self):
+        def fila_a_str(idx):
+            if idx == len(self.matriz):
+                return ""
+            fila_str = "\t".join(map(str, self.matriz[idx]))
+            resto = fila_a_str(idx + 1)
+            return fila_str if resto == "" else fila_str + "\n" + resto
+        return fila_a_str(0)
