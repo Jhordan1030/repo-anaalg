6fa4c9d270187ab70e47dbb0859b9008ecec0634
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2025-07-21 09:27:30 -0500

    Graficar tiempos en linux

--- ExamenGrupal2/graficar.py
+++ ExamenGrupal2/graficar.py
@@ -7,7 +7,7 @@ df = pd.read_csv("ExamenGrupal2/resultados/tiempos.csv", encoding="latin1")
 # Graficar
 plt.figure(figsize=(10, 6))
 for columna in df.columns[1:]:  # Omitimos la columna 'Tamaño'
-    plt.plot(df["Tamaño"], df[columna], marker='o', label=columna)
+    plt.plot(df["Tamano"], df[columna], marker='o', label=columna)
 
 plt.title("Comparación de Tiempos por Tamaño de Arreglo")
 plt.xlabel("Tamaño del Arreglo")
